1298a12f856d086462127d2f803369fd93d9dacd
abdessalam <abdessalam.bahri@esprit.tn>
2026-05-19 11:18:42 +0100

    ML  : earthquake detections

---
+++ ML/EarthquakeWithRF.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+
+
+EQ=pd.read_csv('../datasets/earthquake_alert_balanced_dataset.csv')
+FP=pd.read_csv('../datasets/flight_dataset.csv')
+
+x = EQ[['magnitude','depth', 'cdi','mmi' , 'sig']]
+y = EQ['alert']
+le = LabelEncoder()
+y = le.fit_transform(y)
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+rf = RandomForestClassifier(n_estimators=100, random_state=42,max_depth=8)
+rf.fit(X_train, y_train)
+y_pred = rf.predict(X_test)
+print(classification_report(y_test, y_pred))
+print(confusion_matrix(y_test, y_pred))
+print("Accuracy:", accuracy_score(y_test, y_pred))
+
+magnitude = float(input("Enter the magnitude of the earthquake: "))
+depth = float(input("Enter the depth of the earthquake: "))
+cdi = float(input("Enter the Community Internet Intensity Map (cdi) value: "))
+mmi = float(input("Enter the Modified Mercalli Intensity (mmi) value: "))
+sig = float(input("Enter the significance (sig) value: "))
+
+new_data = pd.DataFrame([[magnitude, depth, cdi, mmi, sig]], columns=['magnitude', 'depth', 'cdi', 'mmi', 'sig'])
+pred_encoded = rf.predict(new_data)[0]
+new_prediction = le.inverse_transform([pred_encoded])[0]
+print("New Prediction:", new_prediction)
